1dbc58daa2e6e651ebec694480386a51d2aceecd
Leonardo Oliveira <lolive4@sibsnet.local>
2021-10-29 15:30:53 +0100

    XML layout changes

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "I'am Showing a Toast!", Toast.LENGTH_LONG).show();
+
     }
+
+
+
 }
