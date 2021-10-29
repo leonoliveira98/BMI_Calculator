a528f586b05b30111fb96ad8b0ba213be1c69f7f
Leonardo Oliveira <lolive4@sibsnet.local>
2021-10-29 18:06:27 +0100

    Section 4 - Android Foundations - 22.

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,9 +18,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String alertText = "This is my variable text";
 
-        Toast.makeText(this, alertText, Toast.LENGTH_LONG).show();
+        // Ir buscar os Id's do XML e mete-los em variaveis
+        TextView resultText = findViewById(R.id.text_view_result);
+
+        RadioButton maleButton = findViewById(R.id.radio_button_female);
+        RadioButton femaleButton = findViewById(R.id.radio_button_male);
+
+        EditText ageEditText = findViewById(R.id.edit_text_age);
+        EditText feetEditText = findViewById(R.id.edit_text_feet);
+        EditText inchesEditText = findViewById(R.id.edit_text_inches);
+        EditText weightEditText = findViewById(R.id.edit_text_weight);
+
+        Button calculateButton = findViewById(R.id.button_calculate);
+
 
     }
 
